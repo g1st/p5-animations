70264a369f1b2f9cc0c5a270e7c3bf0896bd1d2e
g1st <gintstan@gmail.com>
2020-05-15 08:05:15 +0100

    Make circle diameter dependand on screen size.

--- zebra-circle/mySketch.js
+++ zebra-circle/mySketch.js
@@ -1,28 +1,35 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0);
-	noLoop();
-	noStroke();
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noLoop();
+  noStroke();
 }
 
 function draw() {
-	drawArcs(300);
-	
-	// black circles in the middle
-	fill(0);
-	circle(width / 2 - 30, height / 2 + 20, 60);
-	circle(width / 2 + 45, height / 2 - 40, 140);
+  drawArcs(300);
+
+  // black circles in the middle
+  fill(0);
+  circle(width / 2 - 30, height / 2 + 20, 60);
+  circle(width / 2 + 45, height / 2 - 40, 140);
 }
 
 function drawArcs(numOfArcs) {
-	const circleDiameter = 400;
-	for (let i = 0; i < numOfArcs; i++) {
-		if (i % 2 === 0) {
-			fill(0);
-		} else {
-			fill(255);
-		}
-		arc(width / 2, height / 2, circleDiameter, circleDiameter,
-				TWO_PI / numOfArcs * i, TWO_PI / numOfArcs * i + (TWO_PI / numOfArcs));
-	}
+  const smallerDimension = width > height ? height : width;
+  const circleDiameter = smallerDimension * 0.9;
+  for (let i = 0; i < numOfArcs; i++) {
+    if (i % 2 === 0) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    arc(
+      width / 2,
+      height / 2,
+      circleDiameter,
+      circleDiameter,
+      (TWO_PI / numOfArcs) * i,
+      (TWO_PI / numOfArcs) * i + TWO_PI / numOfArcs
+    );
+  }
 }
