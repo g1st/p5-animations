7d1b7eccbd1e76b76ccd4b4760808dcf479423ec
g1st <gintstan@gmail.com>
2020-07-22 10:44:31 +0100

    Add game start/gameover screen

--- asteroidsy/mySketch.js
+++ asteroidsy/mySketch.js
@@ -5,6 +5,7 @@ const palette = {
   background: '#26251C',
 };
 let score = 0;
+let highScore = 0;
 let scaling = 1;
 let cameraPos;
 let radarDiameter;
@@ -45,41 +46,81 @@ function setup() {
 
   cameraPos = createVector(0, 0);
   radarDiameter = Math.min(width, height);
-  spaceShip = new SpaceShip();
-  // numOfAsteroids, spaceBoundary, radarDiameter
-  asteroids = new Asteroids(750, 5000, radarDiameter);
 }
 
 function draw() {
   background(palette.background);
+  if (gameOver) {
+    gameScreen();
+  } else {
+    scale(scaling);
+    displayInfo();
+    spaceShip.showSpeed();
+    translate(-cameraPos.x + width / 2, -cameraPos.y + height / 2);
 
-  scale(scaling);
-  displayInfo();
-  spaceShip.showSpeed();
-  translate(-cameraPos.x + width / 2, -cameraPos.y + height / 2);
+    spaceShip.drawShip();
+    spaceShip.moveShip();
+    spaceShip.drawShots();
+    spaceShip.moveShots();
+    spaceShip.cameraTowards();
 
-  spaceShip.drawShip();
-  spaceShip.moveShip();
-  spaceShip.drawShots();
-  spaceShip.moveShots();
-  spaceShip.cameraTowards();
+    asteroids.draw();
+    asteroids.move();
+  }
+}
 
-  asteroids.draw();
-  asteroids.move();
+function gameStartScreen() {
+  fill('white');
+  noStroke();
+  fill('red');
+  textSize(width / 20);
+  text('ASTEROIDSY', width * 0.3, height * 0.15);
+  fill('white');
+  textSize(width / 40);
+  let info = `
+ENTER - start,
+UP ARROW - thrust,
+LEFT/RIGHT ARROW - rotate,
+SPACE BAR - shoot,
+M - toggle sound
+`;
+  if (spaceShip) {
+    info = `Your score: ${score}
+Your highscore: ${highScore}
+  ${info}`;
+  }
+
+  text(info, width * 0.3, height * 0.35);
+
+  fill(100, 100, 200, 50);
+  stroke(100, 100, 100, 50);
+  strokeWeight(10);
+  rect(width / 2, height * 0.55, width * 0.5, height * 0.6);
+}
+function gameScreen() {
+  if (spaceShip) {
+    highScore = highScore > score ? highScore : score;
+  }
+  gameStartScreen();
 }
 
 function displayInfo() {
   // fps and sound
   fill('white');
   noStroke();
-
+  textSize(12);
   text(
     `FPS: ${int(frameRate())}
 SOUND: ${sound ? 'ON' : 'OFF'}`,
     25,
     25
   );
-  text(`SCORE: ${score}`, width - 100, 25);
+  text(
+    `SCORE: ${score}
+LIVES: ${spaceShip.lives}`,
+    width - 100,
+    25
+  );
   text(`Position: ${int(cameraPos.x)}, ${int(cameraPos.y)}`, 50, height - 50);
 }
 
@@ -99,4 +140,15 @@ function keyPressed() {
     sound = !sound;
     masterVolume(+sound);
   }
+  if (gameOver && keyCode === 13) {
+    initGame();
+  }
+}
+
+function initGame() {
+  score = 0;
+  gameOver = false;
+  spaceShip = new SpaceShip();
+  // numOfAsteroids, spaceBoundary, radarDiameter
+  asteroids = new Asteroids(750, 5000, radarDiameter);
 }
