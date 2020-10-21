7d89d5082f55f0cbb712ad5aee75cc60622122a8
g1st <gintstan@gmail.com>
2020-10-21 09:49:07 +0100

    Change colours

--- distort-grid/mySketch.js
+++ distort-grid/mySketch.js
@@ -1,53 +1,82 @@
 // based on https://inconvergent-sandbox.glitch.me/distort.html
-
+// palette from https://www.colourlovers.com/palette/301154/Influenza
+const palette = '#300030,#480048,#601848,#C04848,#F07241'.split(',');
 const points = [];
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(220);
-	createGridPoints(40, 20);
-	strokeWeight(0.5);
+  createCanvas(windowWidth, windowHeight);
+  createGridPoints(30);
+  strokeWeight(0.5);
 }
 
 function draw() {
-	background(220);
-	drawLinesBetweenPoints(points, 40);
-	distort(40);
+  background(palette[4]);
+  drawLinesBetweenPoints(points, 60);
+  distort(40);
 }
 
-function createGridPoints(numOfPoints, gap) {
-	for (let i = 2; i < numOfPoints; i++) {
-		let row = [];
-		for (let j = 2; j < numOfPoints; j++) {
-			row.push({
-				y: i * gap,
-				x: j * gap
-			});
-		}
-		points.push(row);
-	}
+function createGridPoints(gap) {
+  const numOfPointsX = floor(width / gap) + 1;
+  const numOfPointsY = floor(height / gap) + 2;
+  for (let i = 0; i < numOfPointsX; i++) {
+    let row = [];
+    for (let j = 0; j < numOfPointsY; j++) {
+      row.push({
+        x: i * gap,
+        y: j * gap,
+      });
+    }
+    points.push(row);
+  }
 }
 
 function drawLinesBetweenPoints(points, disconnectDistance) {
-	for (let i = 0; i < points.length - 1; i++) {
-		for (let j = 0; j < points[i].length - 1; j++) {
-			if (dist(points[i + 1][j].x, points[i + 1][j].y, points[i + 1][j + 1].x, points[i + 1][j + 1].y) < disconnectDistance && i + 1 < points.length - 1) {
-				line(points[i + 1][j].x, points[i + 1][j].y, points[i + 1][j + 1].x, points[i + 1][j + 1].y);
-			}
-			if (dist(points[i][j + 1].x, points[i][j + 1].y, points[i + 1][j + 1].x, points[i + 1][j + 1].y) < disconnectDistance && j + 1 < points.length - 1) {
-				line(points[i][j + 1].x, points[i][j + 1].y, points[i + 1][j + 1].x, points[i + 1][j + 1].y);
-			}
-		}
-	}
+  stroke(palette[0]);
+  for (let i = 0; i < points.length - 1; i++) {
+    for (let j = 0; j < points[i].length - 1; j++) {
+      if (
+        dist(
+          points[i + 1][j].x,
+          points[i + 1][j].y,
+          points[i + 1][j + 1].x,
+          points[i + 1][j + 1].y
+        ) < disconnectDistance &&
+        i + 1 < points.length - 1
+      ) {
+        line(
+          points[i + 1][j].x,
+          points[i + 1][j].y,
+          points[i + 1][j + 1].x,
+          points[i + 1][j + 1].y
+        );
+      }
+      if (
+        dist(
+          points[i][j + 1].x,
+          points[i][j + 1].y,
+          points[i + 1][j + 1].x,
+          points[i + 1][j + 1].y
+        ) < disconnectDistance &&
+        j + 1 < points.length - 1
+      ) {
+        line(
+          points[i][j + 1].x,
+          points[i][j + 1].y,
+          points[i + 1][j + 1].x,
+          points[i + 1][j + 1].y
+        );
+      }
+    }
+  }
 }
 
 function distort(distance) {
-	for (let i = 0; i < points.length; i++) {
-		for (let j = 0; j < points[i].length; j++) {
-			if (dist(mouseX, mouseY, points[i][j].x, points[i][j].y) < distance) {
-				points[i][j].x += random(-4, 4);
-				points[i][j].y += random(-4, 4);
-			}
-		}
-	}
+  for (let i = 0; i < points.length; i++) {
+    for (let j = 0; j < points[i].length; j++) {
+      if (dist(mouseX, mouseY, points[i][j].x, points[i][j].y) < distance) {
+        points[i][j].x += random(-4, 4);
+        points[i][j].y += random(-4, 4);
+      }
+    }
+  }
 }
